8c57bc5cf013c7dd96fd94c4f2a295d204622be3
MohammadBohlak <mohammad.bohlak5@gmail.com>
2025-07-18 18:19:18 +0200

    ..

--- src/index.js
+++ src/index.js
@@ -1,33 +1,20 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import Footer from "./components/ui/footer/Footer";
-
-// createRoot(document.getElementById("root")).render(
-//   <BrowserRouter>
-//     <StrictMode>
-//       <Provider store={store}>
-//         <App />
-//       </Provider>
-//     </StrictMode>
-//   </BrowserRouter>
-// );
-
-fetch("/config.json")
-  .then((res) => res.json())
-  .then((config) => {
-    window.appConfig = config;
-    createRoot(document.getElementById("root")).render(
-      <BrowserRouter>
-        <StrictMode>
-          <Provider store={store}>
-            <App />
-          </Provider>
-        </StrictMode>
-      </BrowserRouter>
-    );
-  });
+ 
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </StrictMode>
+    </BrowserRouter>
+  </React.StrictMode>
+);
