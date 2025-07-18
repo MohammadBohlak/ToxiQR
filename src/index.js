253adb647e6cd9ba5cf3dfb6799971d12699eadf
MohammadBohlak <mohammad.bohlak5@gmail.com>
2025-07-19 01:34:43 +0200

    api

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
- 
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
